6b942ff25de1732810b67d14cd4b3eca5303b18a
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-05-30 14:05:42 +0300

    7.1. Challenge 1: Random nothings

--- bin/challenge.dart
+++ bin/challenge.dart
@@ -1,5 +1,21 @@
-import 'package:challenge/challenge.dart' as challenge;
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${challenge.calculate()}!');
+void main() {
+  challenge1();
+}
+
+/// Challenge 1: Random nothings
+///
+/// Write a function that randomly returns `1` or `null`. Assign
+/// the return value of the function to an variable named `result` that
+/// will never be `null`. Give `result` a default of `0` if the function
+/// returns `null`.
+void challenge1() {
+  int? randomNothings() {
+    final isTrue = Random().nextBool();
+    return isTrue ? 1 : null;
+  }
+
+  int result = randomNothings() ?? 0;
+  print(result);
 }
